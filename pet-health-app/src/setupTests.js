fa63c5574ae51f636cc7324c94ef8f0234e8a7ee
vaios panagiotou <vaios.panagiotoy@gmail.com>
2026-01-19 20:35:51 +0200

    wrong

--- pet-health-app/src/setupTests.js
+++ pet-health-app/src/setupTests.js
@@ -1,5 +1,5 @@
-// Το jest-dom προσθέτει custom matchers για έλεγχο DOM κόμβων.
-// Σας επιτρέπει π.χ.:
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
-// Περισσότερα: https://github.com/testing-library/jest-dom
+// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
